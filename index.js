39b82d5ef762d71ea0077bc30c1b14bb204f098c
Aleah Ellis <aleah.is@icloud.com>
2016-03-10 16:20:53 -0500

    added controller,service app now working

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express'); var app = express();
-// respond with "Hello World!" on the homepage
-app.get('/api/hello', function (req, res) { res.send('./lyrics');
-});
-var server = app.listen(3000, function () { var host = server.address().address;
-var port = server.address().port;
-console.log('Example app listening at http://%s:%s', host, port); });
-
-app.use(express.static(__dirname + '/public'))
+var express = require('express'); 
+var app = express();
 
 //gets the stuff from lyrics.js
-var stuff=require('./lyrics')
-console.log(stuff[Math.floor(Math.random()*stuff.length)]);
+var stuff=require('./lyrics');
+app.get('/api/queenbee', function (req, res) { 
+	res.send(stuff[Math.floor(Math.random()*stuff.length)]);
+});
+
+var server = app.listen(3000, function () { 
+	var host = server.address().address;//configures the server - the ip address
+	var port = server.address().port;
+	// console.log('Example app listening at http://%s:%s', host, port);
+ });
+
+app.use(express.static(__dirname + '/public'));
+
+
+
